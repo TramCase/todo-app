cdfdb4e0c1029dd15af8c6523b18bf8cbf417268
Ytzvan Mastino <mastino14@gmail.com>
2025-08-28 17:06:52 -0500

    [coding task] - Ytzvan Mastino Task List

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface Todo {
   id: string;
@@ -9,11 +9,39 @@ interface Todo {
 }
 
 export default function TodoList() {
+  const [taskName, setTaskName] = useState(''); 
+  const [todoList, setTodoList] = useState<Todo[]>([])
+  useEffect( ()=> {
+    console.log("render");
+  }, [])
+
+  const saveTaskName = (e: React.FormEvent) => {
+    console.log(taskName);
+    e.preventDefault();
+
+    const addTodo: Todo = {
+      id: crypto.randomUUID(),
+      title: taskName,
+      completed: false
+    };
+    setTodoList([...todoList, addTodo])
+    setTaskName("");
+  }
+
   return (
     <div className="w-full max-w-md mx-auto">
       <p>Todo Form Placeholder</p>
+      <form onSubmit={saveTaskName} id="taskForm">
+        <input name="task" type='text' value={taskName} onChange={(e) => setTaskName(e.target.value)}></input>
+        <button>add</button>
+      </form>
       <div className="bg-white rounded shadow">
         <p>Todo List Placeholder</p>
+          <ul>
+            {todoList.map( (item) => (
+              <li key={item.id}>{item.title}</li>
+            ))}
+          </ul>
       </div>
     </div>
   );
